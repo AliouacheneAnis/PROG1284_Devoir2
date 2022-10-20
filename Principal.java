5430830bf8316e3b292702b7a50662a3340543d6
AliouacheneAnis <eaalioua01@monccnb.ca>
2022-10-20 09:36:23 -0300

    Version Final

--- Principal.java
+++ Principal.java
@@ -1,18 +1,27 @@
+/*
+  Titre      : PROG1284_Devoir2
+  Auteur     : Anis Aliouachene
+  Date       : 20/10/2022
+  Description: Fonction main pour executer le programme et la class PlusGrandeValeur 
+  Version    : 0.0.1
+*/
+
+// Importation des libraires 
 import java.util.ArrayList;
 
+// declaration class Principal 
 public class Principal {
 
-  public static void main(String[] args) {
-    
-    ArrayList<Integer> Tab = new ArrayList<Integer>();
-    PlusGrandeValeur Tableau = new PlusGrandeValeur(); 
+    // declaration class maion
+    public static void main(String[] args) {
 
-    Tableau.affectation(Tab);
-    Tableau.recuperationMaxEtIndex(Tab);
-    Tableau.affichage();
+        // Declaration arraylist et objet de la class PlusGrandeValeur
+        ArrayList<Integer> Tab = new ArrayList<Integer>();
+        PlusGrandeValeur Tableau = new PlusGrandeValeur();
 
-
-
-
-  }
+        // Traitement et Affichage
+        Tableau.affectation(Tab);
+        Tableau.recuperationMaxEtIndex(Tab);
+        Tableau.affichage(Tab);
+    }
 }
